d72e43beccc8b861bd50e7624e07308a6c66c9fc
​ <74103103+Calvindd2f@users.noreply.github.com>
2024-10-17 13:30:38 +0100

    Commit

--- Rules/ErrorRules.ps1
+++
@@ -1,6 +0,0 @@
-class CheckForUndefinedVariables : ErrorRule {
-    CheckForUndefinedVariables() : base('CheckForUndefinedVariables', 'Check for undefined variables') {}
-
-    [object]Invoke([System.Management.Automation.Language.Ast]$ast) {
-    }
-}
--- Rules/PerformanceRules.ps1
+++
@@ -1,6 +0,0 @@
-class AvoidUsingWriteHost : PerformanceRule {
-   AvoidUsingWriteHost() : base('AvoidUsingWriteHost', 'Avoid using Write-Host') {}
-
-   [object]Invoke([System.Management.Automation.Language.Ast]$ast) {
-   }
-}
